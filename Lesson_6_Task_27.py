231f9ba25d958e557720403ec3ad52d08d5ea943
Viazmenskiy Stanislav <st-51b@mail.ru>
2024-01-29 15:18:57 +0300

    Lesson_6_Task_29.py id added

---
+++ Lesson_6_Task_27.py
@@ -0,0 +1,13 @@
+# Задача Nº27. Пользователь вводит текст(строка). Словом считается последовательность непробельных символов
+# идущих подряд, слова разделены одним или большим числом пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+# Input: She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure. So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells
+# Skillbox proxies
+# Output: 13
+
+stroka = input().split()
+set_1 = set()
+for i in stroka:
+    set_1.add(i.lower())
+print(len(set_1))
+
